0a7074c85e1c186a600b87d7aa1a752fb390be27
Unknown <yarikbur@Mail.ru>
2018-07-26 17:17:12 +0300

    Init

---
+++ desktop/src/ru/sgs/wandering/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.sgs.wandering.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.sgs.wandering.Start;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Start(), config);
+	}
+}
