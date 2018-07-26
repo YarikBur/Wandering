528f0529fa93edacdeccbbcd95a2649c2af4e8a6
Unknown <yarikbur@Mail.ru>
2018-07-26 17:37:13 +0300

    Add main screen

--- desktop/src/ru/sgs/wandering/desktop/DesktopLauncher.java
+++ desktop/src/ru/sgs/wandering/desktop/DesktopLauncher.java
@@ -1,12 +1,35 @@
 package ru.sgs.wandering.desktop;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Locale;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sgstudio.sgs02.utils.Language;
+import com.sgstudio.sgs02.utils.Settings;
+
 import ru.sgs.wandering.Start;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	static String locale;
+	
+	static Language lan;
+	static Settings cfg;
+	
+	public static void main (String[] arg) throws FileNotFoundException, IOException {
+		locale = Locale.getDefault() + "";
+		
+		lan = new Language(locale);
+		cfg = new Settings();
+		Settings.setProperty("language", locale);
+		if(Settings.out())
+			System.out.println(Language.getMessage(2) + locale);
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Wandering";
+		config.width = Integer.parseInt(Settings.getProperty("width"));
+		config.height = Integer.parseInt(Settings.getProperty("height"));
 		new LwjglApplication(new Start(), config);
 	}
 }
