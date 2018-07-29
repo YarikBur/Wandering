dc6e84277544a451546fe5655d04518f92303a93
Unknown <yarikbur@Mail.ru>
2018-07-30 02:10:12 +0300

    Add font.ttf ant gui.png

--- core/src/ru/sgs/wandering/menu/Main.java
+++ core/src/ru/sgs/wandering/menu/Main.java
@@ -3,21 +3,28 @@ package ru.sgs.wandering.menu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.sgstudio.sgs02.utils.Settings;
 
 public class Main implements Screen {
-	
-	
+	SpriteBatch batch;
+	Texture gui;
 	
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
+		batch = new SpriteBatch();
+		gui = new Texture("gui.png");
 	}
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		batch.draw(gui, 0, 0);
+		batch.end();
 	}
 
 	@Override
